9e7f05c24f94442c9efd2918661f838996d414ae
adrisanc <adrian_sc@live.com>
2019-04-15 19:56:42 +0200

    Include modifications in 6_6a.php file

--- 6_6a.php
+++ 6_6a.php
@@ -2,5 +2,5 @@
 
 $users = [3 => 'foo', 24 => 'boB654', 26 => 'billanderson', 45 => 'paulmAc2'];
 
-$finalValues = preg_grep("/^[a-z][a-z]{1}[A-Z]{1}[0-9]{1}/", $users);
+$finalValues = preg_grep("/^[a-z]*[a-z]{1,}[A-Z0-9]{1,}/", $users);
 print_r($finalValues);
