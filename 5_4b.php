951fd42ea947d5a32244367ed2a75b70a1b1b5ae
adrisanc <adrian_sc@live.com>
2019-04-15 19:03:24 +0200

    Include new array in 5_4b.php

--- 5_4b.php
+++ 5_4b.php
@@ -1,15 +1,20 @@
-<?php 
+<?php
 
 include '5_4a.php';
 
 $numeros[] = [
     "num1" => 41,
-    "num2" => 2.1,
+    "num2" => 2.1
 ];
 
 $numeros[] = [
     "num1" => 3,
-    "num2" => 67,
+    "num2" => 67    
+];
+
+$numeros[] = [
+	"num1" => 145,
+	"num2" => 0
 ];
 
 
@@ -21,15 +26,16 @@ $operaciones = [
     'resto' => '%'
 ];
 
-foreach ($numeros as $key => $numero) {
+foreach ($numeros as $key => $numero) {	
 
 	foreach ($operaciones as $key => $operacion) {		
 
 		$result =  calculadora($numero['num1'], $numero['num2'], $key);		
 		
-		if ($result === 'ERROR0') 
+		if ($result == 'ERROR0') 
 			$result = 'Error de división por cero';
 
 		echo $numero["num1"] . ' ' . $operacion . ' ' . $numero["num2"] . ' = ' . $result . '</br>';
+		
 	}
 }
